d9026682cfeff02ff47818629c56567cea57fd53
madara uchiha <notwhiten444@gmail.com>
2026-02-14 13:49:01 +0530

    -

---
+++ learning2/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Photo from './photo.jsx'
+import Built from './built.jsx'
+const Apple = () => {
+  return (
+    <div>To do</div>
+  )
+}
+const App = () => {
+  return (
+    <>
+   <h1><Apple/></h1> 
+    <Photo/>
+    <Built/>
+    </>
+  )
+}
+export default App
+
+
---
+++ learning2/src/photo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+
+const Photo = () => {
+  return (
+    <div>
+      <img src={reactLogo} alt="React Logo" />
+    </div>
+  );
+};
+
+export default Photo;
