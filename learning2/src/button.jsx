d8111a8d6d1df3bf388b3210e8fc2bd997fb8a89
madara uchiha <notwhiten444@gmail.com>
2026-02-20 23:42:45 +0530

    --

--- learning2/src/button.jsx
+++ learning2/src/button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import style from './button.module.css'
 function onclick(){
     alert("hi ; how are you");
 }
 const Button = () => {
   return (<>
-    <button onClick={onclick}> click </button>
+    <button className={style.heading}  onClick={onclick}> click </button>
 </>
   )
 }
